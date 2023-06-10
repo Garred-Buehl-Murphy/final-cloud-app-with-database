eb4f930ecd13a8daa087980e386fcdf1bd194b07
Garred-Buehl-Murphy <garredmurphywork@gmail.com>
2023-06-10 00:54:07 -0400

    still fixing admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -9,17 +9,17 @@ class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-class QuestionInline(admin.StackedInline):
-    model = Question
-    extra = 5
-
 class ChoiceInline(admin.StackedInline):
     model = Choice
+    extra = 5
+
+class QuestionInline(admin.StackedInline):
+    model = Question
     extra = 5
     
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -28,6 +28,8 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
 
 # <HINT> Register Question and Choice models here
 
@@ -35,5 +37,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
