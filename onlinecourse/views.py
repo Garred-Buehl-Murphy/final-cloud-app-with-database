e9cc7f62e779027da2c806e95b4f6b32039ed389
Garred-Buehl-Murphy <garredmurphywork@gmail.com>
2023-06-22 16:00:11 -0400

    mostly done but need to fix some minor errors

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -118,7 +118,7 @@ def submit(request, course_id):
     choices = extract_answers(request)
     submission.choices.set(choices)
     submission_id = submission.id
-    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id,)))
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(course_id, submission_id,)))
 
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
@@ -138,7 +138,16 @@ def extract_answers(request):
         # Get the selected choice ids from the submission record
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
-
-
-
+def show_exam_result(request, course_id, submission_id):
+    context = {}
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
+    total_score = 0
+    for choice in choices:
+        if choice.is_correct:
+            total_score += choice.question_id.grade
+    context['course'] = course
+    context['grade'] = total_score
+    context['choices'] = choices
+    return  render(request, 'onlinecourse/exam_result_bootstrap.html', context)
